aaac36e91cc68a0b5967930ab0797fdbd8e55917
yml <1179739037@qq.com>
2018-03-27 00:25:26 +0800

    小程序组件提交

---
+++ pages/navigator/navigator.js
@@ -0,0 +1,8 @@
+// redirect.js navigator.js
+Page({
+  onLoad: function (options) {
+    this.setData({
+      title: options.title //获取地址栏的title属性
+    })
+  }
+})
---
+++ pages/scrollview/scrollview.js
@@ -0,0 +1,35 @@
+var order = ['red', 'yellow', 'blue', 'green', 'red']
+Page({
+  data: {
+    toView: 'red',
+    scrollTop: 100
+  },
+  upper: function (e) {
+    console.log('上滑');
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log('下滑');
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log('滚动');
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        console.log(this.data.toView);
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
+})
